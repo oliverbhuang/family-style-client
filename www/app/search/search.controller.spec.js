5deaf63f1c40c2ae5c5fff306c56ffe71ebd7fbd
joeycchin <joeycchin@gmail.com>
2016-03-23 16:00:01 -0700

    redone search.controller.spec with mocha framework

--- www/app/search/search.controller.spec.js
+++ www/app/search/search.controller.spec.js
@@ -1,56 +1,46 @@
-// /* jshint -W117, -W030 */
-// describe('SearchController', function () {
-//   var controller,
-//       deferredSearch,
-//       deferredToTablePage,
-//       tablesServiceMock,
-//       yelpServiceMock;
-//
-//   beforeEach(function () {
-//     // Load the App Module to access controllers
-//     module('app');
-//
-//     //stop caching for HTML load error
-//     module(function ($provide, $urlRouterProvider) {
-//       $provide.value('$ionicTemplateCache', function() {});
-//       $urlRouterProvider.deferIntercept();
-//     });
-//
-//     // instantiate the controller and mocks for every test
-//     inject(function ($controller, $q, _$rootScope_) {
-//       deferredSearch = $q.defer();
-//       deferredToTablePage = $q.defer();
-//
-//       // mock services
-//       tablesServiceMock = {
-//         toTablePage: jasmine.createSpy('toTablePage spy')
-//         .and.returnValue(deferredToTablePage.promise)
-//       };
-//
-//       yelpServiceMock = {
-//         yelpSearch: jasmine.createSpy('yelpSearch spy').and.returnValue(deferredSearch.promise),
-//       };
-//
-//       // instantiate SearchController
-//       controller = $controller('SearchController', {
-//         'yelpService': yelpServiceMock,
-//         'tablesService': tablesServiceMock
-//       });
-//
-//       // call methods on the controller for every test
-//       $rootScope = _$rootScope_;
-//       controller.restSearch('rest', 'loc');
-//       controller.tablePage('id');
-//     });
-//   });
-//
-//   describe('#SearchController', function () {
-//     it('should call yelpSearch on yelpService', function () {
-//       expect(yelpServiceMock.yelpSearch).toHaveBeenCalledWith('rest', 'loc');
-//     });
-//
-//     it('should call toTablePage on tablesService', function () {
-//       expect(tablesServiceMock.toTablePage).toHaveBeenCalled();
-//     });
-//   });
-// });
+/* jshint -W117, -W030 */
+describe('SearchController', function () {
+  var controller;
+
+  beforeEach(function () {
+    module('app');
+
+    inject(function ($controller, $q) {
+      var deferred = $q.defer();
+      var promise = deferred.promise;
+
+      tablesServiceMock = {
+        toTablePage: function () {
+          return true;
+        }
+      };
+
+      yelpServiceMock = {
+        yelpSearch: function (rest, loc) {
+          return promise;
+        }
+      };
+
+      sinon.spy(tablesServiceMock, 'toTablePage');
+      sinon.spy(yelpServiceMock, 'yelpSearch');
+
+      controller = $controller('SearchController', {
+        'yelpService': yelpServiceMock,
+        'tablesService': tablesServiceMock
+      });
+    });
+  });
+
+  describe('#SearchController', function () {
+    it('should call yelpSearch on yelpService', function () {
+      controller.restSearch('rest', 'loc', 'form');
+      expect(yelpServiceMock.yelpSearch.calledOnce).to.equal(true);
+    });
+
+    it('should call toTablePage on tablesService', function () {
+      controller.tablePage('id');
+      expect(tablesServiceMock.toTablePage.calledOnce).to.equal(true);
+      controller.tablePage.restore();
+    });
+  });
+});
