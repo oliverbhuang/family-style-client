80041b2d4f7df21bfdc26795ff5cd45e762b38b8
joeycchin <joeycchin@gmail.com>
2016-03-31 16:53:24 -0700

    leadersBoard

--- www/app/leaderBoard/leaderBoard.controller.js
+++ www/app/leaderBoard/leaderBoard.controller.js
@@ -3,10 +3,27 @@
     .module('app')
     .controller('LeaderBoardController', LeaderBoardController);
 
-  LeaderBoardController.$inject = ['getLeaderBoard'];
+  LeaderBoardController.$inject = ['getLeaderBoard', 'tablesService'];
 
-  function LeaderBoardController(getLeaderBoard) {
+  function LeaderBoardController(getLeaderBoard, tablesService) {
     var vm = this;
     vm.restaurants = getLeaderBoard;
+
+    vm.tablePage = function(restaurant) {
+      restaurant.restaurantAddress = {
+        address: [restaurant.address],
+        city: restaurant.city,
+        // jscs:disable
+        state_code: restaurant.state_code, // jshint ignore:line
+        postal_code: restaurant.postal_code // jshint ignore:line
+        // jscs:enable
+      };
+
+      var restaurantObj = {
+        name: restaurant.restaurantName,
+        location: restaurant.restaurantAddress,
+        id: restaurant.yelpId
+      };
+    };
   }
 })();
