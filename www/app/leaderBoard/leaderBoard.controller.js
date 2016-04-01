a4dc19265a0c3e3ca036e7af9dd84deaa13b4700
joeycchin <joeycchin@gmail.com>
2016-03-31 17:39:47 -0700

    restyled popular page

--- www/app/leaderBoard/leaderBoard.controller.js
+++ www/app/leaderBoard/leaderBoard.controller.js
@@ -10,20 +10,25 @@
     vm.restaurants = getLeaderBoard;
 
     vm.tablePage = function(restaurant) {
-      restaurant.restaurantAddress = {
-        address: [restaurant.address],
-        city: restaurant.city,
+      restaurant[0].restaurantAddress = {
+        address: [restaurant[0].address],
+        city: restaurant[0].city,
         // jscs:disable
-        state_code: restaurant.state_code, // jshint ignore:line
-        postal_code: restaurant.postal_code // jshint ignore:line
+        state_code: restaurant[0].state_code, // jshint ignore:line
+        postal_code: restaurant[0].postal_code // jshint ignore:line
         // jscs:enable
       };
 
       var restaurantObj = {
-        name: restaurant.restaurantName,
-        location: restaurant.restaurantAddress,
-        id: restaurant.yelpId
+        name: restaurant[0].restaurantName,
+        location: restaurant[0].restaurantAddress,
+        id: restaurant[0].yelpId,
+        // jscs:disable
+        image_url: restaurant[0].image // jshint ignore:line
+        // jscs:enable
       };
+
+      tablesService.toTablePage(restaurantObj);
     };
   }
 })();
